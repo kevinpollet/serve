58f8c2248fc1cb1e9018b0c500f174e5aa788653
kevinpollet <pollet.kevin@gmail.com>
2020-02-28 11:11:26 +0400

    chore: lint project sources with golangci-lint

--- options.go
+++ options.go
@@ -4,25 +4,25 @@ import "github.com/justinas/alice"
 
 type fileServerOption func(*FileServer)
 
-func Host(host string) fileServerOption {
+func Host(host string) fileServerOption { // nolint
 	return func(fs *FileServer) {
 		fs.host = host
 	}
 }
 
-func Port(port int) fileServerOption {
+func Port(port int) fileServerOption { // nolint
 	return func(fs *FileServer) {
 		fs.port = port
 	}
 }
 
-func Dir(dir string) fileServerOption {
+func Dir(dir string) fileServerOption { // nolint
 	return func(fs *FileServer) {
 		fs.dir = dir
 	}
 }
 
-func Middlewares(middlewares ...alice.Constructor) fileServerOption {
+func Middlewares(middlewares ...alice.Constructor) fileServerOption { // nolint
 	return func(fs *FileServer) {
 		fs.middlewares = middlewares
 	}
