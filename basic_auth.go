69513693b84d8c52863a6398285c9ad17b9d8974
kevinpollet <pollet.kevin@gmail.com>
2020-03-01 18:06:48 +0400

    feat: add basic handler

---
+++ basic_auth.go
@@ -0,0 +1,74 @@
+package serge
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const headerWWWAuthenticate = "WWW-Authenticate"
+
+type basicAuthHandler struct {
+	credentials map[string][]byte
+	next        http.Handler
+}
+
+func NewBasicAuthHandler(reader io.Reader, next http.Handler) (http.Handler, error) {
+	credentials, err := parseCredentials(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return &basicAuthHandler{credentials, next}, nil
+}
+
+func (h *basicAuthHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	user, pass, hasAuth := req.BasicAuth()
+
+	if !hasAuth {
+		rw.Header().Add(headerWWWAuthenticate, "Basic realm=\"serge\"")
+		rw.WriteHeader(http.StatusUnauthorized)
+
+		return
+	}
+
+	if !h.authenticate(user, pass) {
+		rw.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	h.next.ServeHTTP(rw, req)
+}
+
+func (h *basicAuthHandler) authenticate(user, pass string) bool {
+	passwordHash, userExists := h.credentials[user]
+	return userExists && bcrypt.CompareHashAndPassword(passwordHash, []byte(pass)) == nil
+}
+
+func parseCredentials(reader io.Reader) (map[string][]byte, error) {
+	scanner := bufio.NewScanner(reader)
+	credentialPrefixRegexp := regexp.MustCompile(`\$2[axy]*\$`)
+	credentials := make(map[string][]byte)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ":")
+
+		if len(parts) != 2 || !credentialPrefixRegexp.MatchString(parts[1]) {
+			return nil, fmt.Errorf("unsupported password encoding")
+		}
+
+		credentials[parts[0]] = []byte(parts[1])
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return credentials, nil
+}
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/sirupsen/logrus v1.4.2
-	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
+	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d
+	golang.org/x/sys v0.0.0-20200301040627-c5d0d7b4ec88 // indirect
 )
