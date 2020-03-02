07cc90d9a51b5cba96c50abf5a5385da709c14a6
kevinpollet <pollet.kevin@gmail.com>
2020-03-02 15:58:13 +0400

    refactor: clean code

--- server.go
+++ server.go
@@ -3,31 +3,50 @@ package serge
 import (
 	"compress/flate"
 	"compress/gzip"
+	"fmt"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/andybalholm/brotli"
 	"github.com/kevinpollet/serge/log"
 )
 
-const headerLocation = "Location"
+type fileSystem string
+
+func (fs fileSystem) Open(name string) (http.File, error) {
+	rootDir := string(fs)
+	fullName := filepath.Join(rootDir, filepath.FromSlash(name))
+
+	if strings.HasPrefix(filepath.Base(fullName), ".") {
+		return nil, os.ErrNotExist
+	}
+
+	file, err := os.Open(fullName)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
 
 type fileServer struct {
-	root http.FileSystem
+	fileSystem http.FileSystem
 }
 
 func NewFileServer(dir string) http.Handler {
-	return &fileServer{root: http.Dir(dir)}
+	return &fileServer{fileSystem: fileSystem(dir)}
 }
 
-func (fs *fileServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) { // nolint
+func (server *fileServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	indexPageName := "index.html"
 	urlPath := path.Clean(req.URL.Path)
 
-	file, err := fs.root.Open(urlPath)
+	file, err := server.fileSystem.Open(urlPath)
 	if err != nil {
-		toHTTPResponse(rw, err)
+		toResponse(rw, err)
 		return
 	}
 
@@ -35,24 +54,18 @@ func (fs *fileServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) { // 
 
 	fileInfo, err := file.Stat()
 	if err != nil {
-		toHTTPResponse(rw, err)
+		toResponse(rw, err)
 		return
 	}
 
 	if fileInfo.IsDir() {
 		if !strings.HasSuffix(req.URL.Path, "/") {
-			rw.Header().Add(headerLocation, urlPath+"/")
-			rw.WriteHeader(http.StatusMovedPermanently)
+			redirect(rw, fmt.Sprint(req.URL.Path, "/"))
 		} else {
-			req.URL.Path = urlPath + "/index.html"
-			fs.ServeHTTP(rw, req)
+			req.URL.Path = fmt.Sprintf("%s/%s", urlPath, indexPageName)
+			server.ServeHTTP(rw, req)
 		}
 
-		return
-	}
-
-	if strings.HasPrefix(fileInfo.Name(), ".") {
-		toHTTPResponse(rw, os.ErrNotExist)
 		return
 	}
 
@@ -61,7 +74,7 @@ func (fs *fileServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) { // 
 		encodingBrotli, encodingGzip, encodingDeflate, encodingIdentity,
 	)
 	if err != nil {
-		toHTTPResponse(rw, err)
+		toResponse(rw, err)
 		return
 	}
 
@@ -90,7 +103,12 @@ func (fs *fileServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) { // 
 	http.ServeContent(rw, req, fileInfo.Name(), fileInfo.ModTime(), file)
 }
 
-func toHTTPResponse(rw http.ResponseWriter, err error) {
+func redirect(rw http.ResponseWriter, path string) {
+	rw.Header().Add("Location", path)
+	rw.WriteHeader(http.StatusMovedPermanently)
+}
+
+func toResponse(rw http.ResponseWriter, err error) {
 	switch {
 	case os.IsNotExist(err):
 		rw.WriteHeader(http.StatusNotFound)
