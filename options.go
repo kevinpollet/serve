e23c071def98ed0b87c5a452fe4193460ca702b4
kevinpollet <pollet.kevin@gmail.com>
2021-10-24 16:54:09 +0200

    docs: reword comments

--- options.go
+++ options.go
@@ -6,23 +6,24 @@ import (
 	"github.com/justinas/alice"
 )
 
+// Option is the functional option type.
 type Option func(*fileServer)
 
-// WithAutoIndex is a functional option that enables auto index.
+// WithAutoIndex enables auto index.
 func WithAutoIndex() Option {
 	return func(fs *fileServer) {
 		fs.autoIndex = true
 	}
 }
 
-// WithMiddlewares is a functional option that sets the middlewares to apply before serving the request.
+// WithMiddlewares sets the middlewares to apply before serving the request.
 func WithMiddlewares(middlewares ...alice.Constructor) Option {
 	return func(fs *fileServer) {
 		fs.middlewares = middlewares
 	}
 }
 
-// WithErrorHandler is a functional option that sets the Error handler.
+// WithErrorHandler sets the Error handler.
 func WithErrorHandler(errorHandler func(http.FileSystem, http.ResponseWriter, error)) Option {
 	return func(fs *fileServer) {
 		fs.errorHandler = errorHandler
